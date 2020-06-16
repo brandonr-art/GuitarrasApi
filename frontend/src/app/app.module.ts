35c5edb7adbccd315fa9992cde4a5920eb371575
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-06-16 01:33:18 -0500

    Footer a la plantilla

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GuitarrasComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
