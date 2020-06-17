6ddc9d09501499251ee6d1b9b9cac862b85e3e0e
Alarid52 <alarid152@gmail.com>
2020-06-16 22:31:22 -0500

    otra correccion

--- frontend/src/app/services/guitarra.service.ts
+++ frontend/src/app/services/guitarra.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http'
-import { Guitarra}  from '../models/guitarra';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Guitarra}  from '../models/guitarra';
 import {Observable} from 'rxjs';
 
 @Injectable({
