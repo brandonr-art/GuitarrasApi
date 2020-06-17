4b10bbb928cb5764d906cf050c5d6ae0e80b9862
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-06-16 22:32:38 -0500

    correcion ; HTTPClient

--- frontend/src/app/services/guitarra.service.ts
+++ frontend/src/app/services/guitarra.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http'
+import {HttpClient } from '@angular/common/http';
 import { Guitarra}  from '../models/guitarra';
 import {Observable} from 'rxjs';
 
